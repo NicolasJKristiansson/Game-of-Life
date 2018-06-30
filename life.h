c06f4ab0c38cea9bcaf6696c8684995fef57027e
Nicolas J Kristiansson <nicolas.kristiansson@gmail.com>
2018-06-29 19:15:44 -0500

    Add files via upload

---
+++ life.h
@@ -0,0 +1,35 @@
+#ifndef LIFE_H
+#define LIFE_H
+
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <stdlib.h>
+#include <string>
+#include <cmath>
+
+class Life{
+private:
+    int dim;
+
+    std::vector<std::vector<bool> > world;
+
+    bool getAdjCell(int x, int y, int dir);
+
+public:
+    bool wrap;
+
+    Life();
+    Life(int d, bool w);
+
+    bool getCell(int x, int y);
+    int getDim();
+    bool getRefreshedCell(int x, int y);
+    void modCell(int x, int y, bool live);
+    void refreshWorld();
+    void clearWorld();
+    void simWorld(int n);
+
+};
+
+#endif
